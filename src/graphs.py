e7f99bc007c503d4c6b57312757e1a0c412dd17c
Senuri De Silva <ktsdesilva.15@cse.mrt.ac.lk>
2023-04-11 10:11:29 +0800

    Update graphs.py

--- src/graphs.py
+++ src/graphs.py
@@ -220,7 +220,7 @@ def plot_roc(fpr, tpr, height, width, margin, title_text=None):
     fig.update_xaxes(constrain='domain')
     #fig.show()
 
-    fig.update_layout(bargap=0.05, height=height, width=width, title_text=title_text, title_x=0.5,
+    fig.update_layout(height=height, width=width, title_text=title_text, title_x=0.5,
                       margin=dict(t=margin,b=margin)
     )
 
